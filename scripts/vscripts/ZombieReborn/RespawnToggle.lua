2d2ece5c73cb867b27e95cd644d06314578fe137
Hichatu <realhichatu@gmail.com>
2023-04-11 21:07:33 +0100

    formatting

--- scripts/vscripts/ZombieReborn/RespawnToggle.lua
+++ scripts/vscripts/ZombieReborn/RespawnToggle.lua
@@ -12,13 +12,13 @@ end
 
 function ToggleRespawn()
     if bRespawnToggle == false then
-        Convars:SetInt('mp_respawn_on_death_t',0)
-        Convars:SetInt('mp_respawn_on_death_ct',0)
+        Convars:SetInt('mp_respawn_on_death_t', 0)
+        Convars:SetInt('mp_respawn_on_death_ct', 0)
         ScriptPrintMessageChatAll(" \x04[Zombie:Reborn]\x01 Respawning is disabled!")
     end
     if bRespawnToggle == true then
-        Convars:SetInt('mp_respawn_on_death_t',1)
-        Convars:SetInt('mp_respawn_on_death_ct',1)
+        Convars:SetInt('mp_respawn_on_death_t', 1)
+        Convars:SetInt('mp_respawn_on_death_ct', 1)
         ScriptPrintMessageChatAll(" \x04[Zombie:Reborn]\x01 Respawning is enabled!")
     end
 
